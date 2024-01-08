38ed84245fda39688380658f97559cef207df042
hillary-wangui <hillary.maina@student.moringaschool.com>
2024-01-08 19:27:44 +0300

    loops

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,35 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    i = 10
+    while i > -1:
+        print(i)
+        i -= 1
+    print("Happy New Year!")
+happy_new_year()
+
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    squared_list= list()
+    for integer in int_list :
+        squared_list.append(integer ** 2)
+    return(squared_list)
+    
+print(square_integers([1, 2, 3, 4, 5]))
 
 def fizzbuzz():
     # code goes here!
-    pass
+    i = 1
+    while i < 101:
+        if(i % 3 == 0 and i % 5 == 0):
+            print("FizzBuzz")
+        elif(i % 3 == 0):
+            print("Fizz")
+        elif(i % 5 == 0):
+            print("Buzz")
+        else:
+            print(i)
+        i += 1
+    
+fizzbuzz()
